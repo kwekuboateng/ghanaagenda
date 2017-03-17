1807566b2ed5ba8f27a46a74b263f3ec2f4dc7eb
kwekuboateng <fredshavnqweku@gmail.com>
2017-03-17 01:14:41 +0000

    first commit for ghanaagenda bot

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const ghanaagenda = express();
+
+ghanaagenda.set('port', (process.env.PORT || 5000));
+
+ghanaagenda.use(bodyParser.urlencoded({extended: false}));
+ghanaagenda.use(bodyParser.json());
+
+ghanaagenda.get('/', function(req, res){
+	res.send('Welcome buddy')
+})
+
+ghanaagenda.get('/webhook/', function(req, res){
+	if(req.query['hub.verify_token']) ===
+		('my_page_is_my_password'){
+			res.send(req.challenge['hub.challenge'])
+		}
+}) 
